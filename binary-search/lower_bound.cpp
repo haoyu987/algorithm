29d72f57afbf6ecb263189348e8015ab5c051232
Hao Yu <hao.yu.019@gmail.com>
2016-08-05 18:58:02 -0400

    Create lower_bound.cpp

---
+++ binary-search/lower_bound.cpp
@@ -0,0 +1,16 @@
+template <class ForwardIterator, class T>
+  ForwardIterator lower_bound (ForwardIterator first, ForwardIterator last, const T& val)
+{
+  ForwardIterator it;
+  iterator_traits<ForwardIterator>::difference_type count, step;
+  count = distance(first,last);
+  while (count>0)
+  {
+    it = first; step=count/2; advance (it,step);
+    if (*it<val) {                 // or: if (comp(*it,val)), for version (2)
+      first=++it;
+      count-=step+1;
+    }
+    else count=step;
+  }
+  return first;
