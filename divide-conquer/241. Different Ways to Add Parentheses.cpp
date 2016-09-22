d0bfc6eed4e263d1ef22c8b01691e8cbe3f27ae8
Hao Yu <hao.yu.019@gmail.com>
2016-09-21 23:28:25 -0400

    Update 241. Different Ways to Add Parentheses.cpp

--- divide-conquer/241. Different Ways to Add Parentheses.cpp
+++ divide-conquer/241. Different Ways to Add Parentheses.cpp
@@ -28,5 +28,11 @@ def diffWaysToCompute(self, input):
                for a in build(lo, i)
                for b in build(i + 1, hi)]
    return build(0, len(nums) - 1)
-   
+
+
+def diffWaysToCompute(self, input):
+    return [a+b if c == '+' else a-b if c == '-' else a*b
+            for i, c in enumerate(input) if c in '+-*'
+            for a in self.diffWaysToCompute(input[:i])
+            for b in self.diffWaysToCompute(input[i+1:])] or [int(input)]
 */
